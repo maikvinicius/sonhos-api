0be007e7cf69db461c8cebca23442c45313f5fcf
Maik Vinicius <maikmv.mv@gmail.com>
2019-10-18 18:06:51 -0300

    Implement cors to access API in localhost

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const cors = require('cors')
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://teste:maik123@cluster0-jf8xl.mongodb.net/sonhos?retryWrites=true&w=majority', {
@@ -9,7 +10,18 @@ mongoose.connect('mongodb+srv://teste:maik123@cluster0-jf8xl.mongodb.net/sonhos?
 })
 .then( response => console.log('Conectado com sucesso!'))
 .catch( error => console.log('Não foi possível conectar: ', error.message));
+
 mongoose.set('useFindAndModify', false);
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(cors({ credentials: true, origin: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
